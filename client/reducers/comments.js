88c27223bdb8d1851deb20a4ee59c4edb7340fa2
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2018-05-05 17:34:02 +0000

    Update number of photo likes

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,6 +1,5 @@
 // state MUST be the first argument and action always the second argument
 function comments(state = [], action) {
-  console.log(action, state);
   return state;
 }
 
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -8,8 +8,22 @@
 
 // state MUST be the first argument and action always the second argument
 function posts(state = [], action) {
-  console.log(action, state);
-  return state;
+  // all reducers get called, we need to filter what we actually want
+  switch (action.type) {
+    case 'INCREMENT_LIKES':
+
+      // because we're using flat file and not real database,
+      // let's copy all objects before and after the one that we're updating,
+      // then update the one we want and put back the before and after objects
+      const index = action.index;
+      return [
+        ...state.slice(0, index),  // before the one that we're updating
+        {...state[index], likes: state[index].likes + 1},
+        ...state.slice(index + 1), // after the one that we're updating
+      ]
+    default:
+      return state;
+  }
 }
 
 export default posts;
