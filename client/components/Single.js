c9f39eb9ceacd0441c806a1828de7ca6042f48cb
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2018-05-06 09:13:02 +0000

    Display single photo component

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {Link} from 'react-router';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+    const {posts, params} = this.props;
+    const {postId} = params;
+    const index = posts.findIndex((post) => post.code === postId);
+    const post = posts[index];
+
     return (
       <div className="single-photo">
-        I'm the single
+        <Photo index={index} post={post} {...this.props} />
+        <Comments />
       </div>
     )
   }
