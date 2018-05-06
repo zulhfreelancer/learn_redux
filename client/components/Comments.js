fd58b13e0d6ca4316d7933c6a00835a8da1a285a
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2018-05-06 09:30:03 +0000

    Display the comments

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
 const Comments = React.createClass({
+  renderComment(comment, index) {
+    return(
+      <div className="comment" key={index}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    )
+  },
+
   render() {
+    const {postComments} = this.props;
+
     return (
-      <div className="comment">
-        <p>I'm the comments</p>
+      <div className="comments">
+        {postComments.map(this.renderComment)}
+
+        <form ref="commentForm" className="comment-form">
+          <input type="text" ref="author" placeholder="Author" />
+          <input type="text" ref="comment" placeholder="Comment" />
+          <input type="submit" hidden /> {/* to make Enter key to work, we need input type submit */}
+        </form>
       </div>
     )
   }
--- client/components/Single.js
+++ client/components/Single.js
@@ -4,15 +4,16 @@ import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
-    const {posts, params} = this.props;
+    const {posts, params, comments} = this.props;
     const {postId} = params;
     const index = posts.findIndex((post) => post.code === postId);
     const post = posts[index];
+    const postComments = comments[postId] || []; // guard the post with no comments
 
     return (
       <div className="single-photo">
         <Photo index={index} post={post} {...this.props} />
-        <Comments />
+        <Comments postComments={postComments} />
       </div>
     )
   }
